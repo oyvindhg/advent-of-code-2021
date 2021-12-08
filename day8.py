f16918248156c1ce6604882031b94b1e31a97668
Øyvind Harding Gulbrandsen <13216001+oyvindhg@users.noreply.github.com>
2021-12-09 00:47:36 +0900

    Solve day 8 - Problem 1

---
+++ day8.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class SignalEntry:
+    input: List[str]
+    output: List[str]
+
+
+def parse_input(file_input):
+    signal_entries = []
+    for entries in file_input:
+        patterns, out = entries.split('|')
+        signal_entries.append(SignalEntry(patterns.split(), out.split()))
+    return signal_entries
+
+
+with open('input-files/day8.txt') as f:
+    raw_input = f.read().splitlines()
+
+signals = parse_input(raw_input)
+
+unique_count = 0
+for signal in signals:
+    for entry in signal.output:
+        length = len(entry)
+        if length == 2 or length == 3 or length == 4 or length == 7:
+            unique_count += 1
+
+print(unique_count)
