1656187bc0f436d6ed3c22d7bc6eec72df4985b4
Øyvind Harding Gulbrandsen <13216001+oyvindhg@users.noreply.github.com>
2022-08-27 23:28:06 +0900

    Create quantum function draft

--- day21.py
+++ day21.py
@@ -1,3 +1,6 @@
+from dataclasses import dataclass
+
+
 def read_file(filepath):
     with open(filepath) as f:
         rows = f.read().splitlines()
@@ -6,20 +9,13 @@ def read_file(filepath):
     return p1_start, p2_start
 
 
-def roll_dice():
-    result = 0
-    while True:
-        result += 1
-        yield result
-
-
 def calculate_next_position_and_score(current_position, current_score, jumps):
     position = (current_position + jumps) % 10
     score = current_score + (10 if position == 0 else position)
     return position, score
 
 
-def play(p1_start, p2_start):
+def play_deterministic(p1_start, p2_start):
     dice_roll = 0
     p1_score = 0
     p2_score = 0
@@ -39,12 +35,23 @@ def play(p1_start, p2_start):
     return p1_score, p2_score, dice_roll
 
 
-def main():
-    p1_start, p2_start = read_file('input-files/day21.txt')
-    p1_score, p2_score, dice_roll = play(p1_start, p2_start)
-    losing_score = min(p1_score, p2_score)
+def play_quantum(p1_start, p2_start):
+    p1_wins = 0
+    p2_wins = 0
+    count = {(p1_start, p2_start, 0, 0): 1}
 
+    current_state = count.popitem()
+    p1_pos, p2_pos, p1_score, p2_score = current_state.key
+    # first = calculate_next_position_and_score(c)
+
+
+def main():
+    p1_start, p2_start = read_file('input-files/day21_test.txt')
+    p1_score, p2_score, dice_roll = play_deterministic(p1_start, p2_start)
+    losing_score = min(p1_score, p2_score)
     print(f"Problem 1: {losing_score * dice_roll}")
+
+    play_quantum(p1_start, p2_start)
 
 
 if __name__ == "__main__":
