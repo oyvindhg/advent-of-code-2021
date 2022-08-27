8dd563734629c113233c79d939a9f73f66560b78
Øyvind Harding Gulbrandsen <13216001+oyvindhg@users.noreply.github.com>
2022-08-28 02:07:43 +0900

    Solve day 21 part 2

--- day21.py
+++ day21.py
@@ -1,6 +1,3 @@
-from dataclasses import dataclass
-
-
 def read_file(filepath):
     with open(filepath) as f:
         rows = f.read().splitlines()
@@ -24,7 +21,7 @@ def play_deterministic(p1_start, p2_start):
     is_p1_turn = True
 
     while p1_score < 1000 and p2_score < 1000:
-        jumps = 3 * dice_roll + 1 + 2 + 3
+        jumps = (3 * dice_roll + 1 + 2 + 3) % 100
         dice_roll = dice_roll + 3
         if is_p1_turn:
             p1_position, p1_score = calculate_next_position_and_score(p1_position, p1_score, jumps)
@@ -38,20 +35,39 @@ def play_deterministic(p1_start, p2_start):
 def play_quantum(p1_start, p2_start):
     p1_wins = 0
     p2_wins = 0
-    count = {(p1_start, p2_start, 0, 0): 1}
+    win_score = 21
+    state_counts = {(p1_start, 0, p2_start, 0, True): 1}  # (p1_pos, p1_score, p2_pos, p2_score, is_p1_turn)
+    single_turn_outcomes = [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)]  # (dice roll sum, universe splits)
 
-    current_state = count.popitem()
-    p1_pos, p2_pos, p1_score, p2_score = current_state.key
-    # first = calculate_next_position_and_score(c)
+    while state_counts:
+        current_state, count = state_counts.popitem()
+        p1_pos, p1_score, p2_pos, p2_score, is_p1_turn = current_state
+        for roll, splits in single_turn_outcomes:
+            if is_p1_turn:
+                p1_new_pos, p1_new_score = calculate_next_position_and_score(p1_pos, p1_score, roll)
+                if p1_new_score >= win_score:
+                    p1_wins += count * splits
+                else:
+                    new_state = (p1_new_pos, p1_new_score, p2_pos, p2_score, not is_p1_turn)
+                    state_counts[new_state] = state_counts.get(new_state, 0) + count * splits
+            else:
+                p2_new_pos, p2_new_score = calculate_next_position_and_score(p2_pos, p2_score, roll)
+                if p2_new_score >= win_score:
+                    p2_wins += count * splits
+                else:
+                    new_state = (p1_pos, p1_score, p2_new_pos, p2_new_score, not is_p1_turn)
+                    state_counts[new_state] = state_counts.get(new_state, 0) + count * splits
+    return p1_wins, p2_wins
 
 
 def main():
-    p1_start, p2_start = read_file('input-files/day21_test.txt')
+    p1_start, p2_start = read_file('input-files/day21.txt')
     p1_score, p2_score, dice_roll = play_deterministic(p1_start, p2_start)
     losing_score = min(p1_score, p2_score)
     print(f"Problem 1: {losing_score * dice_roll}")
 
-    play_quantum(p1_start, p2_start)
+    p1_wins, p2_wins = play_quantum(p1_start, p2_start)
+    print(f"Problem 2: {max(p1_wins, p2_wins)}")
 
 
 if __name__ == "__main__":
