b8e68aba37440ce7fbb3d34c188dad795792088d
Øyvind Harding Gulbrandsen <13216001+oyvindhg@users.noreply.github.com>
2022-08-29 22:48:39 +0900

    Solve day 22 part 1

--- day22.py
+++ day22.py
@@ -18,14 +18,14 @@ def read_file(filepath):
 
     instructions = []
     for row in rows:
-        is_on, box = row.split(' ')
+        on_or_off, box = row.split(' ')
         x_raw, y_raw, z_raw = box.split(',')
         x = x_raw.split('x=')[1].split('..')
         y = y_raw.split('y=')[1].split('..')
         z = z_raw.split('z=')[1].split('..')
         instructions.append(
             Instruction(
-                is_on=bool(is_on),
+                is_on=bool(on_or_off == "on"),
                 x_min=int(x[0]),
                 x_max=int(x[1]) if len(x) > 1 else int(x[0]),
                 y_min=int(y[0]),
@@ -37,9 +37,27 @@ def read_file(filepath):
     return instructions
 
 
+def initialize(instructions):
+    lower_bound = -50
+    upper_bound = 50
+    turned_on = set()
+
+    for instruction in instructions:
+        for x in range(max(instruction.x_min, lower_bound), min(instruction.x_max, upper_bound) + 1):
+            for y in range(max(instruction.y_min, lower_bound), min(instruction.y_max, upper_bound) + 1):
+                for z in range(max(instruction.z_min, lower_bound), min(instruction.z_max, upper_bound) + 1):
+                    point = (x, y, z)
+                    if instruction.is_on:
+                        turned_on.add(point)
+                    else:
+                        turned_on.discard(point)
+    return turned_on
+
+
 def main():
-    instructions = read_file('input-files/day22_test.txt')
-    print(instructions)
+    instructions = read_file('input-files/day22.txt')
+    turned_on = initialize(instructions)
+    print(f"Problem 1: {len(turned_on)}")
 
 
 if __name__ == "__main__":
